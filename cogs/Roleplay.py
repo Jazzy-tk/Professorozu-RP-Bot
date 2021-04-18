d02252d05f2894111ccce1ed02754c9339bc9450
Jazzy <toko.kiroshima@gmail.com>
2021-04-18 14:58:06 +0200

    Used the random responses system

--- cogs/Roleplay.py
+++ cogs/Roleplay.py
@@ -2,13 +2,21 @@ import discord
 
 from discord.ext import commands
 
+import random
+
 class Roleplay(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['Good morning!', 'good morning!'])
-    async def Gm(self, ctx):
-        await ctx.send('*Good morning master... How was you sleep?~*')
+    @commands.command(name='Good')
+    async def Gm(self, ctx, *, context):
+        context = ['morning Professorozu!',
+                'morning professorozu! how are you?']
+        responses = ['Good morning master... How was your sleep?',
+                'Good morning master... My sleep was very bad...',
+                'Good morning master! Are we up for programming today?']
+
+        await ctx.send(f'{random.choice(responses)}')
 
 def setup(client):
     client.add_cog(Roleplay(client))
